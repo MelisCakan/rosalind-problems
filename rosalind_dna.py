527a88f19ebac17cb05df43ad6e047df8c270568
MelisCakan <cakanmelis27@gmail.com>
2025-03-18 11:22:41 +0300

    rosalind_dna.py has been written

---
+++ rosalind_dna.py
@@ -0,0 +1,10 @@
+dataset = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+a = dataset.count("A")
+t = dataset.count("T")
+c = dataset.count("C")
+g = dataset.count("G")
+
+print(a, c, g, t)
+
+
